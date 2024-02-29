8c6b0b0f39d86fee522013c98ac1bd2350481a8c
BlackHeart024 <chintankp15@gmail.com>
2024-02-29 11:16:09 +0530

    Commiting Feb_28_Task

---
+++ Feb_28_Task/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Feb_28_Task;
+
+class DivNumbers
+{
+    int ans;
+    DivNumbers()
+    {
+        ans = 0;
+    }
+    public void Division(int n1, int n2)
+    {
+        try
+        {
+            ans = n1 / n2;
+        }
+        catch(DivideByZeroException e)
+        {
+            Console.WriteLine("Exception caught: {0}",e);
+        }
+        finally
+        {
+            Console.WriteLine("Result: {0}",ans);
+        }
+    }
+    static void Main(string[] args)
+    {
+        DivNumbers obj = new DivNumbers();
+        obj.Division(20, 0);
+    }
+}
