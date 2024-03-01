6ccc2d01987f0eade02ccf470d2d0a6c7d6d7840
BlackHeart024 <chintankp15@gmail.com>
2024-03-01 11:56:05 +0530

    Commiting Feb_29_Task

---
+++ Feb_29_Task/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Feb_29_Task;
+
+public delegate void WorkPerformedHandler(int hours, WorkType workType);
+class EventsAndDelegates
+{
+    static void Main(string[] args)
+    {
+        WorkPerformedHandler del1 = new WorkPerformedHandler(Manager_WorkPerformed);
+        del1+=Manager_complete;
+        del1(10,WorkType.Golf);
+        del1(20,WorkType.GotoMeetings);
+        del1(5, WorkType.GenericReports);
+    }
+
+    public static void Manager_WorkPerformed(int workHours, WorkType wType)
+    {
+        Console.WriteLine("Work Performed by Event Handler");
+        Console.WriteLine($"Work hours: {workHours}, Work Type: {wType}");
+    }
+    public static void Manager_complete(int workHours, WorkType wType)
+    {
+        Console.WriteLine("Work completd");
+    }
+
+}
+
+public enum WorkType
+{
+    Golf,
+    GotoMeetings,
+    GenericReports
+}
