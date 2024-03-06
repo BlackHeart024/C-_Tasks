eda8b1b451825f61b0090ce361e9d6c16344da17
BlackHeart024 <chintankp15@gmail.com>
2024-03-06 10:58:03 +0530

    Commiting Mar_05_Task

---
+++ Mar_05_Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System;
+
+namespace Mar_05_Task;
+
+class ThreadingDemo
+{
+    static void Main(string[] args)
+    {
+        Thread t1 = new Thread(DisplayNumbers);
+        t1.Start();
+    }
+    static void DisplayNumbers()
+    {
+        for(int i=1; i<=5; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(5000);
+        }
+        Console.WriteLine("Thread Closed");
+    }
+}
